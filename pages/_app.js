230315218de18dc68bc1df94f638ecc92315ab22
raphael Abracos <raphael_batista_10@hotmail.com>
2021-04-10 14:58:51 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import 'antd/dist/antd.css'
 import '../styles/vars.css'
 import '../styles/global.css'
-
+import Layout from '../components/Layout'
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
